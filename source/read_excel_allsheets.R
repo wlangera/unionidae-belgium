e62ba5a5edbfcdcb3ed9ce16c8444deef4f1652d
wlangera <ward.langeraert@inbo.be>
2024-03-10 13:23:01 +0100

    remove empty sheets

--- source/read_excel_allsheets.R
+++ source/read_excel_allsheets.R
@@ -1,7 +1,17 @@
 read_excel_allsheets <- function(filename) {
+  require(readxl)
+
+  # Get sheets
   sheets <- readxl::excel_sheets(filename)
-  out <- lapply(sheets, function(sh) readxl::read_excel(filename, sheet = sh))
-  names(out) <- sheets
+
+  # Read sheets
+  data_list <- lapply(sheets, function(sh) {
+    readxl::read_excel(filename, sheet = sh)
+  })
+  names(data_list) <- sheets
+
+  # Remove empty sheets
+  out <- data_list[sapply(data_list, function(x) dim(x)[1]) > 0]
 
   return(out)
 }
